db5958ccc11b1f30a2a805af6d7c8c814acc304d
pedro felipe <pedrofelipinho@gmail.com>
2024-04-22 11:13:27 -0300

    Ajustando o cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais";
 import Cabecalho from "./componentes/Cabecalho";
-import CampoTexto from "./componentes/CampoTexto";
 
 const FundoGradiente = styled.div`
 background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -15,7 +14,6 @@ function App() {
     <FundoGradiente>
       <EstilosGlobais />
       <Cabecalho />
-      <CampoTexto/>
     </FundoGradiente>
   )
 }
--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import CampoTexto from "../CampoTexto";
 
 const HeaderEstilizado = styled.header`
     padding: 60px 0;
@@ -12,6 +13,7 @@ const HeaderEstilizado = styled.header`
 const Cabecalho = () => {
     return (<HeaderEstilizado>
         <img src="/imagens/logo.png" alt="Logo Space App" />
+        <CampoTexto />
     </HeaderEstilizado>)
 }
 
