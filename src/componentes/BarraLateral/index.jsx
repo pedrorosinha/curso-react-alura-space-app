369df5b77f3b944460ad9aa287e735cbc3fa5963
pedro felipe <pedrofelipinho@gmail.com>
2024-04-22 11:24:24 -0300

    Começando a barra lateral

---
+++ src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`
+
+const BarraLateral = () => {
+    return (
+        <aside>
+            <nav>
+                <ListaEstilizada>
+                    <li>
+                        <a href="">Ínicio</a>
+                    </li>
+                    <li>
+                        <a href="">Mais vistas</a>
+                    </li>
+                </ListaEstilizada>
+            </nav>
+        </aside>
+    )
+}
+
+export default BarraLateral;
