c793292bb43f16a9ea625c5c84a71ca8880b9025
pedro felipe <pedrofelipinho@gmail.com>
2024-04-23 15:57:50 -0300

    Adicionando o modal do zoom das fotos da galeria já com a opacidade

---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
+const ModalZoom = ({ foto }) => {
+    return (
+        <>
+            {foto && <>
+                <Overlay />
+                <DialogEstilizado open={!!foto}>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <button>OK</button>
+                    </form>
+                </DialogEstilizado>
+            </>}
+        </>
+    )
+}
+
+export default ModalZoom;
