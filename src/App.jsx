16e19bafc50a7f13dba6e52f38c98cc67e4128e9
pedro felipe <pedrofelipinho@gmail.com>
2024-04-23 15:06:27 -0300

    Ajustando a galeria

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/Banner";
 import bannerBackground from './assets/banner.png';
 import Galeria from "./componentes/Galeria";
+import fotos from './fotos.json'
+import { useState } from "react";
 
 const FundoGradiente = styled.div`
 background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -29,8 +31,8 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
-
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -43,7 +45,7 @@ function App() {
               texto={"A galeria mais completa de fotos do espaço"}
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria} />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
